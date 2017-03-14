92b572a5a991b87fb45abb081b4bf396c2a4672c
Jakub Cizek <jcz@hud.cz>
2017-03-14 13:19:35 +0100

    Created demo project

--- ScrollableDisclaimerAlert/ViewController.swift
+++ ScrollableDisclaimerAlert/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, DisclaimerViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var disclaimer = DisclaimerView.initView()
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var killSwitch: UISwitch!
+    
+    // MARK: Buttons
+    
+    @IBAction func loadContentFromFile(_ sender: Any) {
+        disclaimer = DisclaimerView.initWith(title: "Disclaimer", filePath: "test", fileExtension: "txt", delegate: self)
+        self.view.addSubview(disclaimer)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func loadContentAsString(_ sender: Any) {
+        disclaimer = DisclaimerView.initWith(title: "Škoda", content: "Škoda Auto (Czech pronunciation: [ˈʃkoda]), more commonly known as Škoda, is a Czech automobile manufacturer founded in 1895 as Laurin & Klement. It is headquartered in Mladá Boleslav, Czech Republic. Škoda Auto (Czech pronunciation: [ˈʃkoda]), more commonly known as Škoda, is a Czech automobile manufacturer founded in 1895 as Laurin & Klement. It is headquartered in Mladá Boleslav, Czech Republic. Škoda Auto (Czech pronunciation: [ˈʃkoda]), more commonly known as Škoda, is a Czech automobile manufacturer founded in 1895 as Laurin & Klement. It is headquartered in Mladá Boleslav, Czech Republic. Škoda Auto (Czech pronunciation: [ˈʃkoda]), more commonly known as Škoda, is a Czech automobile manufacturer founded in 1895 as Laurin & Klement. It is headquartered in Mladá Boleslav, Czech Republic. Škoda Auto (Czech pronunciation: [ˈʃkoda]), more commonly known as Škoda, is a Czech automobile manufacturer founded in 1895 as Laurin & Klement. It is headquartered in Mladá Boleslav, Czech Republic. Škoda Auto (Czech pronunciation: [ˈʃkoda]), more commonly known as Škoda, is a Czech automobile manufacturer founded in 1895 as Laurin & Klement. It is headquartered in Mladá Boleslav, Czech Republic. ", delegate: self)
+        self.view.addSubview(disclaimer)
     }
-
-
+    
+    // MARK: DisclaimerViewDelegate
+    
+    func acceptDisclaimer() {
+        print("acceptDisclaimer")
+        disclaimer.removeFromSuperview()
+        statusLabel.text = "ACCEPTED"
+    }
+    
+    func cancelDisclaimer() {
+        print("cancelDisclaimer")
+        disclaimer.removeFromSuperview()
+        if killSwitch.isOn { exit(0) } else { disclaimer.removeFromSuperview(); statusLabel.text = "CANCELED" }
+    }
 }
-
---
+++ ScrollableDisclaimerAlert/disclaimer/DisclaimerView.swift
@@ -0,0 +1,157 @@
+//
+//  DisclaimerView.swift
+//  ScrollableDisclaimerViewAlert
+//
+//  Created by Kuba on 09/03/2017.
+//  Copyright © 2017 HuD. All rights reserved.
+//
+
+import UIKit
+
+protocol DisclaimerViewDelegate: class {
+    func acceptDisclaimer()
+    func cancelDisclaimer()
+}
+
+class DisclaimerView: UIView {
+
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var acceptButton: UIButton!
+    
+    @IBOutlet weak var content: UITextView!
+    @IBOutlet weak var title: UILabel!
+    
+    let kAnimationDuration = 0.3
+    
+    // MARK: Delegate
+    
+    weak var delegate: DisclaimerViewDelegate?
+
+    @IBAction func cancelAction(_ sender: Any) {
+        delegate?.cancelDisclaimer()
+    }
+    
+    @IBAction func acceptAction(_ sender: Any) {
+        delegate?.acceptDisclaimer()
+    }
+    
+    // MARK: Implementation
+    
+    class func initView() -> DisclaimerView {
+        
+        let disclaimerView = UINib(nibName: "Disclaimer", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! DisclaimerView
+        return disclaimerView
+    }
+    
+    class func initWith(title: String, content: String, cancelTitle: String = "Cancel", acceptTitle: String = "Confirm", frame: CGRect = UIScreen.main.bounds, delegate: DisclaimerViewDelegate) -> DisclaimerView {
+        
+        let disclaimerView = UINib(nibName: "Disclaimer", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! DisclaimerView
+        
+        disclaimerView.title.text = title
+        disclaimerView.content.text = content
+        disclaimerView.acceptButton.setTitle(acceptTitle, for: .normal)
+        disclaimerView.cancelButton.setTitle(cancelTitle, for: .normal)
+        
+        disclaimerView.delegate = delegate
+        
+        disclaimerView.frame = frame
+        
+        disclaimerView.cancelButton.borders(for: [.top, .right], color: .init(colorLiteralRed: 0.9, green: 0.9, blue: 0.9, alpha: 1))
+        disclaimerView.acceptButton.borders(for: [.top], color: .init(colorLiteralRed: 0.9, green: 0.9, blue: 0.9, alpha: 1))
+        
+        disclaimerView.backgroundColor = .lightGray
+        
+        return disclaimerView
+    }
+    
+    class func initWith(title: String, filePath: String, fileExtension: String, cancelTitle: String = "Cancel", acceptTitle: String = "Confirm", frame: CGRect = UIScreen.main.bounds, delegate: DisclaimerViewDelegate) -> DisclaimerView {
+        
+        let disclaimerView = UINib(nibName: "Disclaimer", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! DisclaimerView
+        
+        disclaimerView.title.text = title
+
+        if let path = Bundle.main.path(forResource: filePath, ofType: fileExtension) {
+            disclaimerView.content.text = try? String(contentsOfFile: path, encoding: String.Encoding.utf8)
+        }
+        
+        disclaimerView.acceptButton.setTitle(acceptTitle, for: .normal)
+        disclaimerView.cancelButton.setTitle(cancelTitle, for: .normal)
+        
+        disclaimerView.delegate = delegate
+        
+        disclaimerView.frame = frame
+        
+        disclaimerView.cancelButton.borders(for: [.top, .right], color: .init(colorLiteralRed: 0.9, green: 0.9, blue: 0.9, alpha: 1))
+        disclaimerView.acceptButton.borders(for: [.top], color: .init(colorLiteralRed: 0.9, green: 0.9, blue: 0.9, alpha: 1))
+        
+        disclaimerView.backgroundColor = .lightGray
+        
+        return disclaimerView
+    }
+    
+    override func layoutSubviews() {
+        layer.opacity = 0
+        UIView.animate(withDuration: kAnimationDuration) {
+            self.layer.opacity = 1
+        }
+    }
+    
+    override func removeFromSuperview() {
+        UIView.animate(withDuration: kAnimationDuration, animations: {
+            self.layer.opacity = 0
+        }) { processing in
+            if !processing { super.removeFromSuperview() }
+        }
+    }
+}
+
+// MARK: - Extenstions
+
+extension UIView {
+    func borders(for edges:[UIRectEdge], width:CGFloat = 1, color: UIColor = .black) {
+        
+        if edges.contains(.all) {
+            layer.borderWidth = width
+            layer.borderColor = color.cgColor
+        } else {
+            let allSpecificBorders:[UIRectEdge] = [.top, .bottom, .left, .right]
+            
+            for edge in allSpecificBorders {
+                if let v = viewWithTag(Int(edge.rawValue)) {
+                    v.removeFromSuperview()
+                }
+                
+                if edges.contains(edge) {
+                    let v = UIView()
+                    v.tag = Int(edge.rawValue)
+                    v.backgroundColor = color
+                    v.translatesAutoresizingMaskIntoConstraints = false
+                    addSubview(v)
+                    
+                    var horizontalVisualFormat = "H:"
+                    var verticalVisualFormat = "V:"
+                    
+                    switch edge {
+                    case UIRectEdge.bottom:
+                        horizontalVisualFormat += "|-(0)-[v]-(0)-|"
+                        verticalVisualFormat += "[v(\(width))]-(0)-|"
+                    case UIRectEdge.top:
+                        horizontalVisualFormat += "|-(0)-[v]-(0)-|"
+                        verticalVisualFormat += "|-(0)-[v(\(width))]"
+                    case UIRectEdge.left:
+                        horizontalVisualFormat += "|-(0)-[v(\(width))]"
+                        verticalVisualFormat += "|-(0)-[v]-(0)-|"
+                    case UIRectEdge.right:
+                        horizontalVisualFormat += "[v(\(width))]-(0)-|"
+                        verticalVisualFormat += "|-(0)-[v]-(0)-|"
+                    default:
+                        break
+                    }
+                    
+                    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: horizontalVisualFormat, options: .directionLeadingToTrailing, metrics: nil, views: ["v": v]))
+                    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: verticalVisualFormat, options: .directionLeadingToTrailing, metrics: nil, views: ["v": v]))
+                }
+            }
+        }
+    }
+}
